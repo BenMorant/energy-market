6d893567bebd44140cea398074a6369368e6e227
Benjamin Morant <morantbenjamin@gmail.com>
2023-11-22 15:47:19 +0100

    create springinitialzr skeleton

---
+++ src/main/java/com/agregio/energymarket/EnergyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.agregio.energymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agregio/energymarket/EnergyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agregio.energymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
